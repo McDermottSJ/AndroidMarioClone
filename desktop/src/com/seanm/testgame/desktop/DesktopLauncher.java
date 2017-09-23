af80cf17df74c59b46fc002b5c9260d0cc76441e
Sean McDermott <mcdermottsj@vcu.edu>
2017-09-23 14:29:35 -0400

    initialization

---
+++ desktop/src/com/seanm/testgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seanm.testgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seanm.testgame.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
