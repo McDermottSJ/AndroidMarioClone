24d3647308f452def0bf1bc7c66d42b7ad23be75
Sean McDermott <mcdermottsj@vcu.edu>
2017-09-23 14:49:57 -0400

    Added HUD

--- core/src/com/seanm/testgame/Screens/PlayScreen.java
+++ core/src/com/seanm/testgame/Screens/PlayScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.seanm.testgame.MyGdxGame;
+import com.seanm.testgame.Scenes.Hud;
 
 /**
  * Created by sean on 9/23/17.
@@ -17,15 +18,15 @@ import com.seanm.testgame.MyGdxGame;
 
 public class PlayScreen implements Screen {
     private MyGdxGame game;
-    Texture texture;
     private OrthographicCamera gameCam;
     private Viewport gamePort;
+    private Hud hud;
 
     public PlayScreen(MyGdxGame game){
         this.game = game;
-        texture = new Texture("badlogic.jpg");
         gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(800,480,gameCam);
+        gamePort = new FitViewport(MyGdxGame.V_WIDTH,MyGdxGame.V_HEIGHT,gameCam);
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -37,10 +38,8 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(gameCam.combined);
-        game.batch.begin();
-        game.batch.draw(texture,0,0);
-        game.batch.end();
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
     }
 
     @Override
